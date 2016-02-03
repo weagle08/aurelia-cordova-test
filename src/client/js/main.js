71654b4ccbd96d1404e9a130b004c3b810d7abf3
ben johnson <weagle08@gmail.com>
2016-02-02 20:57:42 -0600

    initial

---
+++ src/client/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ben on 2/2/16.
+ */
+export function configure(aurelia) {
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging();
+
+    //Uncomment the line below to enable animation.
+    //aurelia.use.plugin('aurelia-animator-css');
+    //if the css animator is enabled, add swap-order="after" to all router-view elements
+
+    //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    //aurelia.use.plugin('aurelia-html-import-template-loader')
+
+    aurelia.start().then(a => a.setRoot());
+}
